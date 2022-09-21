d0cd243e28a091e20ce8515d3ba1deacbf1a2ae4
Megasix <75232015+Megasix@users.noreply.github.com>
2022-09-21 15:06:38 -0400

    first commit and mod_bits update

--- INF147 TP1/mod_bits.cpp
+++ INF147 TP1/mod_bits.cpp
@@ -1,78 +1,85 @@
 #include "mod_bits.h"
+#include "math.h"
 
 /* À remplir */
 
-/*
-void test_BITS_obtenir(void)
-{
+unsigned int BITS_obtenir(unsigned int valeur, unsigned int position) {
+    return (valeur >> (position-1)) & 1;
+}
 
-	printf("\nTEST_BITS_OBTENIR\n");
+void BITS_basculer(unsigned int *valeur, unsigned int position) {
+    *valeur ^= (1 << (position-1));
+}
 
-	const unsigned int une_valeur = 2863311530;
+unsigned int BITS_extraire(unsigned int valeur, unsigned int bit_depart, unsigned int bit_fin) {
+    unsigned int mask = pow(2,bit_fin)- pow(2,bit_depart-1);
+    return valeur & mask;
+}
 
-	printf("\n\tTest no.1 - Afficher les bits de 2,863,311,530 en petit boutien\n\n");
-	printf("\t\tReponse attendue : 1010 1010 1010 1010 1010 1010 1010 1010\n");
-	printf("\t\tReponse obtenue  : ");
 
-	int compteur = 1;
+void test_BITS_obtenir() {
 
-	for (int i = UINT_NB_BITS; i >= 1; i--)
-	{
+    printf("\nTEST_BITS_OBTENIR\n");
 
-		printf("%i", BITS_obtenir(une_valeur, i));
+    const unsigned int une_valeur = 2863311530;
 
-		if (compteur % 4 == 0)
-		{
-			printf(" ");
-		}
+    printf("\n\tTest no.1 - Afficher les bits de 2,863,311,530 en petit boutien\n\n");
+    printf("\t\tReponse attendue : 1010 1010 1010 1010 1010 1010 1010 1010\n");
+    printf("\t\tReponse obtenue  : ");
 
-		compteur++;
+    int compteur = 1;
 
-	}
+    for (int i = UINT_NB_BITS; i >= 1; i--) {
 
-	printf("\n");
+        printf("%i", BITS_obtenir(une_valeur, i));
+
+        if (compteur % 4 == 0) {
+            printf(" ");
+        }
+
+        compteur++;
+
+    }
+
+    printf("\n");
 
 }
 
-void test_BITS_basculer(void)
-{
+void test_BITS_basculer() {
 
-	printf("\nTEST_BITS_BASCULER\n");
+    printf("\nTEST_BITS_BASCULER\n");
 
-	const unsigned int valeur_attendue_1 = 4294967295;
-	unsigned int valeur_obtenue_1 = 2863311530;
+    const unsigned int valeur_attendue_1 = 4294967295;
+    unsigned int valeur_obtenue_1 = 2863311530;
 
-	for (int i = 1; i <= UINT_NB_BITS - 1; i = i + 2)
-	{
-		BITS_basculer(&valeur_obtenue_1, i);
-	}
+    for (int i = 1; i <= UINT_NB_BITS - 1; i = i + 2) {
+        BITS_basculer(&valeur_obtenue_1, i);
+    }
 
-	printf("\n\tTest no.1 - Basculer les bits pairs de 2863311530\n\n");
-	printf("\t\tReponse attendue : %u\n", valeur_attendue_1);
-	printf("\t\tReponse obtenue  : %u\n", valeur_obtenue_1);
+    printf("\n\tTest no.1 - Basculer les bits pairs de 2863311530\n\n");
+    printf("\t\tReponse attendue : %u\n", valeur_attendue_1);
+    printf("\t\tReponse obtenue  : %u\n", valeur_obtenue_1);
 
 }
 
-void test_BITS_extraire(void)
-{
+void test_BITS_extraire() {
 
-	printf("\nTEST_BITS_EXTRAIRE\n");
+    printf("\nTEST_BITS_EXTRAIRE\n");
 
-	const unsigned int une_valeur = 2863311530;
+    const unsigned int une_valeur = 2863311530;
 
-	unsigned int valeur_attendue_1 = 10;
-	unsigned int valeur_obtenue_1 = BITS_extraire(une_valeur, 1, 4);
+    unsigned int valeur_attendue_1 = 10;
+    unsigned int valeur_obtenue_1 = BITS_extraire(une_valeur, 1, 4);
 
-	printf("\n\tTest no.1 - Afficher les bits 1 -> 4 de 2,863,311,530\n\n");
-	printf("\t\tReponse attendue : %u\n", valeur_attendue_1);
-	printf("\t\tReponse obtenue  : %u\n", valeur_obtenue_1);
+    printf("\n\tTest no.1 - Afficher les bits 1 -> 4 de 2,863,311,530\n\n");
+    printf("\t\tReponse attendue : %u\n", valeur_attendue_1);
+    printf("\t\tReponse obtenue  : %u\n", valeur_obtenue_1);
 
-	unsigned int valeur_attendue_2 = 5;
-	unsigned int valeur_obtenue_2 = BITS_extraire(une_valeur, 2, 5);
+    unsigned int valeur_attendue_2 = 5;
+    unsigned int valeur_obtenue_2 = BITS_extraire(une_valeur, 2, 5);
 
-	printf("\n\tTest no.2 - Afficher les bits 2 -> 5 de 2,863,311,530\n\n");
-	printf("\t\tReponse attendue : %u\n", valeur_attendue_2);
-	printf("\t\tReponse obtenue  : %u\n", valeur_obtenue_2);
+    printf("\n\tTest no.2 - Afficher les bits 2 -> 5 de 2,863,311,530\n\n");
+    printf("\t\tReponse attendue : %u\n", valeur_attendue_2);
+    printf("\t\tReponse obtenue  : %u\n", valeur_obtenue_2);
 
 }
-*/
