e8ce0136419c1580de5b2dae565b5073161df22a
TeanoRocha <113925520+TeanoRocha@users.noreply.github.com>
2022-09-28 13:24:13 -0400

    Update mod_math.h

--- INF147 TP1/mod_math.h
+++ INF147 TP1/mod_math.h
@@ -1,6 +1,17 @@
 #ifndef __MOD_MATH__
 #define __MOD_MATH__
 
-/* À remplir */
+const double MATH_EPSILON = 0.0001;
+const double PI = 3.1415926535897932;
+
+/* Ã€ remplir */
+
+void test_MATH_puissance(void);
+
+
+void test_MATH_racine_carree(void);
+
+
+void test_MATH_sin(void);
 
 #endif
