507c8437e7bab803c0480a843d6b909db10d71ce
Megasix <75232015+Megasix@users.noreply.github.com>
2022-10-09 12:23:09 -0400

    Update mod_traceur.cpp

--- INF147 TP1/mod_traceur.cpp
+++ INF147 TP1/mod_traceur.cpp
@@ -5,4 +5,20 @@ void TRACEUR_initialiser_fenetre(void)
 	initwindow(DIM_LARGEUR, DIM_HAUTEUR);
 }
 
-/* À remplir */
+void TRACEUR_ligne(int x_1, int y_1, int x_2, int y_2){
+    setcolor(WHITE);
+    line(x_1, y_1, x_2, y_2);
+    setcolor(LIGHTCYAN);
+    circle(x_1, y_1, R_FAISCEAU);
+    circle(x_2, y_2, R_FAISCEAU);
+
+    if(x_2-x_1==0 && y_2-y_1!=0){
+        line(x_1, y_1+R_FAISCEAU, x_2, y_2+R_FAISCEAU);
+        line(x_1, y_1-R_FAISCEAU, x_2, y_2-R_FAISCEAU);
+    }else if (x_2-x_1!=0 && y_2-y_1==0){
+        line(x_1+R_FAISCEAU, y_1, x_2+R_FAISCEAU, y_2);
+        line(x_1-R_FAISCEAU, y_1, x_2-R_FAISCEAU, y_2);
+    }else{
+
+    }
+}
