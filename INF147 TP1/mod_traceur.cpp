0885b19898fb97a0dca5ac03586bcccdbf2083e3
Megasix <75232015+Megasix@users.noreply.github.com>
2022-10-09 15:06:33 -0400

    Update mod_traceur.cpp

--- INF147 TP1/mod_traceur.cpp
+++ INF147 TP1/mod_traceur.cpp
@@ -7,20 +7,24 @@ void TRACEUR_initialiser_fenetre()
 }
 
 void TRACEUR_ligne(int x_1, int y_1, int x_2, int y_2){
+    int delta_x = (int) MATH_valeur_absolue((double)x_1-x_2);
+    int delta_y = (int) MATH_valeur_absolue((double)y_1-y_2);
+
+
     setcolor(WHITE);
     line(x_1, y_1, x_2, y_2);
     setcolor(LIGHTCYAN);
     circle(x_1, y_1, R_FAISCEAU);
     circle(x_2, y_2, R_FAISCEAU);
 
-    if(x_2-x_1==0 && y_2-y_1!=0){
+    if(delta_x==0 && delta_y!=0){
         line(x_1, y_1+R_FAISCEAU, x_2, y_2+R_FAISCEAU);
         line(x_1, y_1-R_FAISCEAU, x_2, y_2-R_FAISCEAU);
-    }else if (x_2-x_1!=0 && y_2-y_1==0){
+    }else if (delta_x!=0 && delta_y==0){
         line(x_1+R_FAISCEAU, y_1, x_2+R_FAISCEAU, y_2);
         line(x_1-R_FAISCEAU, y_1, x_2-R_FAISCEAU, y_2);
     }else{
-        double angle = MATH_atan((double) (y_2-y_1)/(x_2-x_1));
+        double angle = MATH_atan((double) (delta_y)/(delta_x));
         int dplc_x = (int) MATH_cos(angle)*R_FAISCEAU;
         int dplc_y = (int) MATH_sin(angle)*R_FAISCEAU;
         if(y_1>y_2){
