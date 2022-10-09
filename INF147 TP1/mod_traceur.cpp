9744a7b14d4a6e76632fdb5fa3bcc8fac50eaad4
Megasix <75232015+Megasix@users.noreply.github.com>
2022-10-09 13:53:07 -0400

    Update mod_traceur.cpp

--- INF147 TP1/mod_traceur.cpp
+++ INF147 TP1/mod_traceur.cpp
@@ -1,4 +1,5 @@
 #include "mod_traceur.h"
+#include "mod_math.h"
 
 void TRACEUR_initialiser_fenetre()
 {
@@ -19,6 +20,15 @@ void TRACEUR_ligne(int x_1, int y_1, int x_2, int y_2){
         line(x_1+R_FAISCEAU, y_1, x_2+R_FAISCEAU, y_2);
         line(x_1-R_FAISCEAU, y_1, x_2-R_FAISCEAU, y_2);
     }else{
-
+        double angle = MATH_atan((double) (y_2-y_1)/(x_2-x_1));
+        int dplc_x = (int) MATH_cos(angle)*R_FAISCEAU;
+        int dplc_y = (int) MATH_sin(angle)*R_FAISCEAU;
+        if(y_1>y_2){
+            line(x_1+dplc_x, y_1+dplc_y, x_2+dplc_x, y_2+dplc_y);
+            line(x_1-dplc_x, y_1-dplc_y, x_2-dplc_x, y_2-dplc_y);
+        } else {
+            line(x_1+dplc_x, y_1-dplc_y, x_2+dplc_x, y_2-dplc_y);
+            line(x_1-dplc_x, y_1+dplc_y, x_2-dplc_x, y_2+dplc_y);
+        }
     }
 }
