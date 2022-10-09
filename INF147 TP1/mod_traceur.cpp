e3595af7d42029365bad6d255e71eca9106c2e6e
TeanoRocha <113925520+TeanoRocha@users.noreply.github.com>
2022-10-09 14:08:25 -0400

    Update mod_traceur.cpp

--- INF147 TP1/mod_traceur.cpp
+++ INF147 TP1/mod_traceur.cpp
@@ -57,8 +57,8 @@ void TRACEUR_ligne(int x_1, int y_1, int x_2, int y_2)
 		circle(x_2, y_2, rayon);
 		break;
 	case 3 :
-		int dx = 0;
-		int dy = 0;
+		int dx = cos(MATH_atan(delta_y / delta_x)) * rayon;
+		int dy = sin(MATH_atan(delta_y / delta_x)) * rayon;
 
 		setcolor(WHITE);
 		line(x_1, y_1, x_2, y_2);
@@ -68,17 +68,11 @@ void TRACEUR_ligne(int x_1, int y_1, int x_2, int y_2)
 
 		if (delta_y < 0) //si vrai : ligne oblique montant
 		{
-			dx = cos(MATH_atan(delta_y / delta_x)) * rayon;
-			dy = sin(MATH_atan(delta_y / delta_x)) * rayon;
-		
 			line(x_1 + dx, y_1 - dy, x_2 + dx, y_2 - dy);
 			line(x_1 - dx, y_1 + dy, x_2 - dx, y_2 + dy);
 		}
 		else //sinon c'est descendant
 		{
-			dx = cos(MATH_atan(delta_y / delta_x)) * rayon;
-			dy = sin(MATH_atan(delta_y / delta_x)) * rayon;
-
 			line(x_1 + dx, y_1 + dy, x_2 + dx, y_2 + dy);
 			line(x_1 - dx, y_1 - dy, x_2 - dx, y_2 - dy);
 		}
