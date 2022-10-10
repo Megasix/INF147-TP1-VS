91faab83e4a4f0d9aca1c9ba2c60b0541a47a09c
PhilippeM <philippe.marineau@hotmail.com>
2022-10-09 23:18:12 -0400

    Added Header and comment in mod_outils

--- INF147 TP1/mod_outils.h
+++ INF147 TP1/mod_outils.h
@@ -1,8 +1,27 @@
+/*****************************************************************************************
+Auteurs :	Barry Lawson
+			Philippe Marineau
+			Jean-Philippe Portant
+			Teano Rocha
+Date : Octobre 2022
+
+Outils pouvant etre utilises a differents endroits dans le programme
+
+On y retrouve la macro fonction suivante :
+	- SWAP : Permute deux valeurs
+*****************************************************************************************/
+
 #ifndef __MOD_OUTILS__
 #define __MOD_OUTILS__
 
-#define SWAP(X, Y)		*X = *X ^ *Y;\
-				*Y = *X ^ *Y;\
-				*X = *X ^ *Y;
+/*****************************************************************************************
+*                                   MACRO-FONCTIONS                                      *
+*****************************************************************************************/
+
+
+// Macro-fonction qui permute deux valeurs
+#define SWAP(X, Y)	*X = *X ^ *Y;\
+					*Y = *X ^ *Y;\
+					*X = *X ^ *Y;
 
 #endif
